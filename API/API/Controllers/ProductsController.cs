715e2a688beb04a1b4ad89cccd38b102b6cbc667
ADIL MABROUK <78666859+adilmabrouk1@users.noreply.github.com>
2021-04-20 05:03:49 +0000

    Initial Commit

---
+++ API/API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Infastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        StoreDBContext _storeDBContext;
+
+        public ProductsController(StoreDBContext storeDBContext)
+        {
+            _storeDBContext = storeDBContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _storeDBContext.products.ToListAsync();
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _storeDBContext.products.FindAsync(id);
+            return product;
+        }
+    }
+}
---
+++ Infastructure/Data/StoreDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infastructure.Data
+{
+    public class StoreDBContext : DbContext
+    {
+        public StoreDBContext(DbContextOptions<StoreDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> products { get; set; }
+    }
+}
